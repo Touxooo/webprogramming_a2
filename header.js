337c26337dcff0ddf091d2827c3227e2b5324a98
Touxooo <thomas.rabiet@epitech.eu>
2024-05-15 15:19:03 +0700

    [MOD] Login button

--- header.js
+++ header.js
@@ -1,25 +1,29 @@
-const setAuthButtonState = () => {
-  const loginButton = document.getElementById("authButton");
-
+const setAuthButtonValues = () => {
   const access_token = localStorage.getItem("access_token");
 
   if (access_token) {
     loginButton.className = "btn btn-danger";
     loginButton.innerHTML = "Logout";
-    loginButton.addEventListener("click", () => {
-      localStorage.removeItem("access_token");
-      alert("Successfuly logout");
-      setAuthButtonState();
-      loginButton.removeEventListener();
-    });
   } else {
     loginButton.className = "btn btn-outline-success";
     loginButton.innerHTML = "Login";
-    loginButton.addEventListener("click", () => {
-      location.replace("/authentication/login.html");
-      loginButton.removeEventListener();
-    });
   }
 };
 
-setAuthButtonState();
+const loginButton = document.getElementById("authButton");
+
+const access_token = localStorage.getItem("access_token");
+
+loginButton.addEventListener("click", () => {
+  const access_token = localStorage.getItem("access_token");
+
+  if (access_token) {
+    localStorage.removeItem("access_token");
+    alert("Successfuly logout");
+  } else {
+    location.replace("/authentication/login.html");
+  }
+  setAuthButtonValues();
+});
+
+setAuthButtonValues();
